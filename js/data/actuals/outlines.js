ddb5cc0557144318e0e9d9c92f1e10f6abcad03f
Sergey Tsibulskiy <sergets@yandex-team.ru>
2018-04-04 17:38:37 +0300

    Change default coloring

--- js/data/actuals/outlines.js
+++ js/data/actuals/outlines.js
@@ -10,6 +10,8 @@ define([
     katzColoring
 ) {
 
+var DEFAULT_COLORING = 'katz';
+
 var colorings = {
     now : nowColoring,
     sobyanin : sobyaninColoring,
@@ -18,9 +20,9 @@ var colorings = {
 
 return {
     shouldRecalc : function(state, updatedStateFields) {
-        var coloring = colorings[state.customColoringId || 'now'];
+        var coloring = colorings[state.customColoringId || DEFAULT_COLORING];
 
-        return updatedStateFields.indexOf('customColoringId') != -1 || 
+        return updatedStateFields.indexOf('customColoringId') != -1 ||
             coloring.shouldRecalcOutlinesOn.some(function(stateField) {
                 return updatedStateFields.indexOf(stateField) != -1;
             });
@@ -29,7 +31,7 @@ return {
     deps : ['widths', 'routes'],
 
     calc : function(data, state, widths, routes) {
-        var coloring = colorings[state.customColoringId || 'now'];
+        var coloring = colorings[state.customColoringId || DEFAULT_COLORING];
 
         return vow.resolve(Object.keys(data.segments).reduce(function(outlines, segmentId) {
             outlines[+segmentId] = coloring.getSegmentOutlines(+segmentId, data, state, {
